7f22fd088881b145fe92027b5ee5e274f7d17aab
allenmurfee <allenmurfee123@gmail.com>
2024-03-19 17:30:42 -0400

    added flex properties and margin

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default function App() {
         <Button title="Add Goal" />
       </View>
       {/* List view for all goals */}
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of Goals...</Text>
       </View>
     </View>
@@ -25,12 +25,14 @@ const styles = StyleSheet.create({
   appContainer: {
     paddingTop: 100,
     paddingHorizontal: 16,
+    flex: 1,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    paddingBottom: 24,
+    marginBottom: 24,
     borderBottomWidth: 1,
     borderBottomColor: "#cccccc",
   },
@@ -40,5 +42,8 @@ const styles = StyleSheet.create({
     width: "70%",
     marginRight: 8,
     padding: 8,
+  },
+  goalsContainer: {
+    flex: 4,
   },
 });
